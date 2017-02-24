60063dae0fd7e5d9fb0e2c737ad1a03ee5c3b1df
borrys <andrzej.kimel@gmail.com>
2017-02-24 15:39:53 +0100

    system init for ubuntu

---
+++ ubuntu-init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get install zsh vim tmux fonts-powerline 
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+chsh -s `which zsh`
+
+./zsh-init.sh
+./vim-init.sh
+./clojure-init.sh
